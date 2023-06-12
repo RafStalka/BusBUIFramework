e5045900059a271f553bd5d79f7723b0150c90fe
rafalst <rastal@saveatrain.com>
2023-06-12 19:44:29 +0200

    date attempt

--- src/test/java/com/busb_ui_framework/GeneralDataTest.java
+++ src/test/java/com/busb_ui_framework/GeneralDataTest.java
@@ -4,10 +4,7 @@ import com.busb_ui_framework.driverConfig.DriverFactory;
 import com.busb_ui_framework.general.Configuration;
 import com.busb_ui_framework.pages.MainPage;
 import io.qameta.allure.Description;
-import org.openqa.selenium.By;
-import org.openqa.selenium.OutputType;
-import org.openqa.selenium.TakesScreenshot;
-import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.*;
 import org.openqa.selenium.io.FileHandler;
 import org.testng.ITestResult;
 import org.testng.Reporter;
@@ -17,9 +14,13 @@ import org.testng.annotations.Test;
 
 import java.io.File;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
 import java.time.Duration;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.Date;
+import java.util.List;
 
 import static com.busb_ui_framework.general.ConfigurationManager.configuration;
 import static com.busb_ui_framework.pages.MainPage.getMainPage;
@@ -64,18 +65,28 @@ public class GeneralDataTest {
 
     @Description("Taken data with first date")
     @Test
-    public void pricesFirstDate() {
+    public void pricesFirstDate() throws InterruptedException {
 
         driver.findElement(By.id("affiliate_checkbox")).click();
-        driver.findElement(By.id("origin-c1ty-input")).click();
-        driver.findElement(By.cssSelector(".search-jss32")).click();
-        driver.findElement(By.id("origin-c1ty-input")).sendKeys("MEININGER Hotel Berlin Central Station");
-        driver.findElement(By.id("destination-c1ty-input")).click();
-        driver.findElement(By.xpath("//div[@id=\'option-37206215\']/div")).click();
-        driver.findElement(By.id("destination-c1ty-input")).sendKeys("Hamburg Airport");
-        driver.findElement(By.id("outbound-date-input")).click();
-        driver.findElement(By.xpath("//button[12]/div/div")).click();
-        driver.findElement(By.xpath("//div[2]/button/span")).click();
+
+        WebElement sourceInput = driver.findElement(By.id("origin-c1ty-input"));
+        WebElement destinationInput = driver.findElement(By.id("destination-c1ty-input"));
+
+        // Enter source and destination values
+        sourceInput.sendKeys("Source City");
+        destinationInput.click();
+        destinationInput.sendKeys("Destination City");
+
+        // Select the date two days in the future
+        LocalDate currentDate = LocalDate.now();
+        LocalDate futureDate = currentDate.plusDays(2);
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        String futureDateStr = formatter.format(futureDate);
+
+        WebElement dateInput = driver.findElement(By.id("outbound-date-input"));
+        dateInput.click();
+        Thread.sleep(3000);
+        dateInput.sendKeys(futureDateStr);
 
     }
 }
